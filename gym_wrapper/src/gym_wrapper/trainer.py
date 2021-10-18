9ee963034fd3d64aeb0f6b009025681636da98c0
ademord <druidelf3@gmail.com>
2021-10-18 18:38:49 +0200

    Update trainer.py

--- gym_wrapper/src/gym_wrapper/trainer.py
+++ gym_wrapper/src/gym_wrapper/trainer.py
@@ -165,7 +165,7 @@ class Trainer:
             self.rank += num_cpu
 
             with SubprocVecEnv([
-                self._make_my_vec_env(_rank=self.config.env_id,
+                self._make_my_vec_env(_rank=i,
                                       _wandb_run_identifier=i,
                                       _callback=self.callback) for i in range(idx_r, num_cpu + idx_r)
             ]) as env:
